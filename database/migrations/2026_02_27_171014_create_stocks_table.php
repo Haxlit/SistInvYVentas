b85eabf39d17e4eca18b50a79fad4c78de708388
Josue <josueadhemarmh@gmail.com>
2026-02-27 17:21:31 -0400

    Proyecto Final

---
+++ database/migrations/2026_02_27_171014_create_stocks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained();
+            $table->integer('available_quantity');
+            $table->decimal('purchase_price', 18, 2);
+            $table->decimal('sale_price', 18, 2);
+            $table->dateTime('entry_date')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stocks');
+    }
+};
